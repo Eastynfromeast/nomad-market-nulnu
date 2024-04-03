e13bb0a63973d70fcb2c8bb5d49afe24a66d5220
Eastyn <eastynwork@gmail.com>
2024-04-03 19:00:01 +0900

    Add eslint-plugin-tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
-      <div>
-        <span>🥕</span>
-        <h1>Carrot</h1>
-        <h2>Welcome to Carrot Market</h2>
-      </div>
-      <div>
-        <Link href="/create-account">시작하기</Link>
-        <div>
-          <span>이미 계정이 있으신가요?</span>
-          <Link href="/login">로그인</Link>
-        </div>
-      </div>
-    </div>
+		<div className="flex flex-col items-center justify-between min-h-screen p-6">
+			<div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+				<span className="text-9xl">🥕</span>
+				<h1 className="text-4xl">Carrot</h1>
+				<h2 className="text-2xl">Welcome to Carrot Market!</h2>
+			</div>
+			<div className="flex flex-col items-center gap-3 w-full">
+				<Link
+					href="/create-account"
+					className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+				>
+					시작하기
+				</Link>
+				<div className="flex gap-1">
+					<span>이미 계정이 있으신가요?</span>
+					<Link href="/login" className="hover:underline underline-offset-4">
+						로그인
+					</Link>
+				</div>
+			</div>
+		</div>
 	);
 }
